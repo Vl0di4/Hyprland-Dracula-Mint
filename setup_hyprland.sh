3b81437a326665fcd136f6209ce8214313543af2
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2025-06-02 07:05:54 +0300

    Create setup_hyprland.sh

---
+++ setup_hyprland.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# Устанавливаем все программы
+sudo pacman -S wayland xorg xorg-server xorg-xwayland hyprland xdg-desktop-portal-hyprland kitty neovim firefox pavucontrol nemo grim slurp swappy rofi rofi-calc rofi-emoji waybar mako ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-firacode-nerd noto-fonts-emoji otf-font-awesome xarchiver hyprpicker btop calcurse telegram-desktop discord wlsunset geoclue2 python3 swayimg swaybg nodejs npm eza zsh fuse2 wl-clipboard mc udiskie vlc unzip flatpak --noconfirm
+
+# Настройка zsh (настройка тем НЕ автоматическая)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm ~/.zshrc
+cp ~/Hyprland-Dracula-Mint/Dracula-Mint/.zshrc ~/
+chsh -s /usr/bin/zsh
+
+#Создание папки .config и .themes
+mkdir -p ~/.config
+mkdir -p ~/.themes
+
+# Копируем конфигурационные файлы после установки ПО
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/bgimages ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/hypr ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/waybar ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/kitty ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/rofi ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/calcurse ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/gtk-3.0 ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/gtk-4.0 ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/swappy ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/neofetch ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/nvim ~/.config/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/mako ~/.config/
+
+# переносим настройки курсора
+cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/.icons ~/
+
+#Hастройка фаерфокса
+mkdir -p ~/.mozilla/firefox/n0eij4j3.default-release
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/firefox/profiles.ini ~/.mozilla/firefox/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/firefox/user.js ~/.mozilla/firefox/n0eij4j3.default-release/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/firefox/chrome ~/.mozilla/firefox/n0eij4j3.default-release/
+
+#Добавление GTK темы
+tar -xf ~/Hyprland-Dracula-Mint/Dracula-Mint/Dracula-mint-GTK.tar.gz
+tar -xf ~/Hyprland-Dracula-Mint/Dracula-Mint/assets.tar.gz
+cp -rf Dracula-mint-GTK ~/.themes/
+cp -rf assets ~/.config/
+
+#Установка yay и нужных программ + тема курсора
+git clone https://aur.archlinux.org/yay.git ~/yay
+cd ~/yay
+makepkg -si
+yay -S bibata-cursor-theme-bin
+
+# Настройка Дискорда
+curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
+chmod +x betterdiscordctl
+sudo mv betterdiscordctl /usr/local/bin
+sudo betterdiscordctl self-upgrade
+betterdiscordctl install
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/BetterDiscord ~/.config/
+## Посте этого в настройках ДС будет доступна тема
+
+# Настраиваем права на выполнение
+chmod +x ~/.config/rofi/network-manager.sh
+chmod +x ~/.config/rofi/rofi-power-menu.sh
+chmod +x ~/.config/hypr/show_layout_popup.sh
+chmod +x ~/.config/hypr/hyprpicker.sh
