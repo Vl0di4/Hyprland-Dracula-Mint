2b85c929d473a481478e9fc2c2df8fae30619669
Vl0di4 <134379671+Vl0di4@users.noreply.github.com>
2025-06-12 16:36:44 +0300

    Update setup_hyprland.sh

--- setup_hyprland.sh
+++ setup_hyprland.sh
@@ -3,15 +3,6 @@
 # Устанавливаем все программы
 sudo pacman -S wayland xorg xorg-server xorg-xwayland hyprland xdg-desktop-portal-hyprland kitty neovim firefox pavucontrol nemo grim slurp swappy rofi rofi-calc rofi-emoji waybar mako ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-firacode-nerd noto-fonts-emoji otf-font-awesome xarchiver hyprpicker btop calcurse telegram-desktop discord wlsunset geoclue2 python3 swayimg swaybg nodejs npm eza zsh fuse2 wl-clipboard mc udiskie vlc unzip flatpak --noconfirm
 
-# Настройка zsh (настройка тем НЕ автоматическая)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-rm ~/.zshrc
-cp ~/Hyprland-Dracula-Mint/Dracula-Mint/.zshrc ~/
-chsh -s /usr/bin/zsh
-
 #Создание папки .config и .themes
 mkdir -p ~/.config
 mkdir -p ~/.themes
@@ -31,7 +22,7 @@ cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/nvim ~/.config/
 cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/mako ~/.config/
 
 # переносим настройки курсора
-cp -rf ~/BSPWM-Dracula-Mint/Dracula-Mint/.icons ~/
+cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/.icons ~/
 
 #Hастройка фаерфокса
 mkdir -p ~/.mozilla/firefox/n0eij4j3.default-release
@@ -51,13 +42,6 @@ cd ~/yay
 makepkg -si
 yay -S bibata-cursor-theme-bin
 
-# Настройка Дискорда
-curl -O https://raw.githubusercontent.com/bb010g/betterdiscordctl/master/betterdiscordctl
-chmod +x betterdiscordctl
-sudo mv betterdiscordctl /usr/local/bin
-sudo betterdiscordctl self-upgrade
-betterdiscordctl install
-cp -rf ~/Hyprland-Dracula-Mint/Dracula-Mint/BetterDiscord ~/.config/
 ## Посте этого в настройках ДС будет доступна тема
 
 # Настраиваем права на выполнение
@@ -65,3 +49,12 @@ chmod +x ~/.config/rofi/network-manager.sh
 chmod +x ~/.config/rofi/rofi-power-menu.sh
 chmod +x ~/.config/hypr/show_layout_popup.sh
 chmod +x ~/.config/hypr/hyprpicker.sh
+
+# Настройка zsh (настройка тем НЕ автоматическая)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+rm ~/.zshrc
+cp ~/Hyprland-Dracula-Mint/Dracula-Mint/.zshrc ~/
+chsh -s /usr/bin/zsh
